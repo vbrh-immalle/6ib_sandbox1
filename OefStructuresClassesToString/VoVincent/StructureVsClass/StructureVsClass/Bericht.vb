efc1217d74c986738be8d9934b396dc629c11ba8
Dinges <vincent.voortmans@immalle.be>
2012-11-07 09:48:22 +0100

    Datum added

--- OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Bericht.vb
+++ OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Bericht.vb
@@ -1,9 +1,16 @@
 ﻿Public Class Bericht
     Property afzender As String
     Property bericht As String
+    Property datum As DateTime
 
     Public Overrides Function ToString() As String
 
-        Return "Afzender: " & afzender & vbCrLf & "Bericht: " & bericht
+        Return "Afzender: " & afzender & vbCrLf & "Bericht: " & bericht & vbCrLf & "Datum: " & datum
     End Function
+
+    Sub Verzend()
+        Console.WriteLine("Bericht wordt verzonden")
+    End Sub
+
+
 End Class
--- OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Module1.vb
+++ OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Module1.vb
@@ -3,14 +3,17 @@
     Sub Main()
         ' Maak hier 2 nieuwe berichten aan en druk ze af met Console.WriteLine (en de ToString)-method
         ' Denk eraan: structures zijn VALUE types!
+
         Dim b1 As New Bericht
         b1.afzender = "barry"
         b1.bericht = "saispas"
+        b1.datum = Now
         Console.WriteLine(b1.ToString())
 
         Dim b2 As New Bericht
         b2.afzender = "Walter"
         b2.bericht = "dinges"
+        b2.datum = New DateTime(2013, 7, 1)
         Console.WriteLine(b2.ToString())
 
 
