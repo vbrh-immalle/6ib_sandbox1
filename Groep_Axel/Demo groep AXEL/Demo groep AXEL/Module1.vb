e25947552d90e7879881d8eb92f26adeeb460d00
Bram <bram.hanssens@immalle.be>
2012-11-07 09:18:43 +0100

    betere BMI

--- Groep_Axel/Demo groep AXEL/Demo groep AXEL/Module1.vb
+++ Groep_Axel/Demo groep AXEL/Demo groep AXEL/Module1.vb
@@ -1,23 +1,17 @@
 ﻿Module Module1
 
+    Function BerekenBMI(ByVal lengte As Double, ByVal gewicht As Double) As Double
+        Return gewicht / lengte ^ 2
+    End Function
+
+    Function BerekenBMIAsStrings(ByVal lengte As String, ByVal gewicht As String) As String
+        Return BerekenBMI(Double.Parse(lengte), Double.Parse(gewicht)).ToString()
+    End Function
+
     Sub Main()
-        Dim lengte, gewicht, bmi As Double
-        Dim lengte2, gewicht2, bmi2 As Double
-
-
-        Console.WriteLine("Voer eerst lengte en daarna het gewicht in van persoon 1")
-        lengte = Double.Parse(Console.ReadLine())
-        gewicht = Double.Parse(Console.ReadLine())
-        Console.WriteLine()
-        Console.WriteLine("Voer eerst lengte en daarna het gewicht in van persoon 2")
-        lengte2 = Double.Parse(Console.ReadLine())
-        gewicht2 = Double.Parse(Console.ReadLine())
-
-        bmi = gewicht / lengte ^ 2
-        bmi2 = gewicht2 / lengte2 ^ 2
-
-        Console.WriteLine("bmi persoon 1: " & bmi.ToString())
-        Console.WriteLine("bmi persoon 2: " & bmi2.ToString())
+        Console.WriteLine("Geef eerst lengte daarna gewicht in.")
+        Console.WriteLine("bmi persoon 1: " & BerekenBMIAsStrings(Console.ReadLine(), Console.ReadLine()))
+        Console.WriteLine("bmi persoon 2: " & BerekenBMIAsStrings(Console.ReadLine(), Console.ReadLine()))
 
         Console.ReadLine()
     End Sub
