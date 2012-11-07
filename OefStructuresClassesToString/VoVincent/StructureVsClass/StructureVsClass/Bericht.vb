a25f5916aa370353a2a56d5ba30cff1eebb5ef6b
Dinges <vincent.voortmans@immalle.be>
2012-11-07 09:40:56 +0100

    Structure bericht -> class

---
+++ OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Bericht.vb
@@ -0,0 +1,9 @@
+﻿Public Class Bericht
+    Property afzender As String
+    Property bericht As String
+
+    Public Overrides Function ToString() As String
+
+        Return "Afzender: " & afzender & vbCrLf & "Bericht: " & bericht
+    End Function
+End Class
--- OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Module1.vb
+++ OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Module1.vb
@@ -1,28 +1,14 @@
-﻿Structure Bericht
-    Dim afzender As String
-    Dim bericht As String
-
-    Public Overrides Function ToString() As String
-
-        Return "Afzender: " & afzender & vbCrLf & "Bericht: " & bericht
-    End Function
-    ' Override hier de ToString() method van object die afzender en bericht afprint
-    ' b.v. 
-    ' Afzender: Jos
-    ' Bericht: Waar ben je?
-End Structure
-
-Module Module1
+﻿Module Module1
 
     Sub Main()
         ' Maak hier 2 nieuwe berichten aan en druk ze af met Console.WriteLine (en de ToString)-method
         ' Denk eraan: structures zijn VALUE types!
-        Dim b1 As Bericht
+        Dim b1 As New Bericht
         b1.afzender = "barry"
         b1.bericht = "saispas"
         Console.WriteLine(b1.ToString())
 
-        Dim b2 As Bericht
+        Dim b2 As New Bericht
         b2.afzender = "Walter"
         b2.bericht = "dinges"
         Console.WriteLine(b2.ToString())
