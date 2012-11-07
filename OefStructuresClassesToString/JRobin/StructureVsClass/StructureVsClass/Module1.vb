bc4ad033c556df60cbc65a69a536458f158a1bd7
Robie Konijn nv <robin.janssens@immalle.be>
2012-11-07 09:52:27 +0100

    Structure Bericht -> Class Bericht
    Structure verandert naar Class

--- OefStructuresClassesToString/JRobin/StructureVsClass/StructureVsClass/Module1.vb
+++ OefStructuresClassesToString/JRobin/StructureVsClass/StructureVsClass/Module1.vb
@@ -1,18 +1,18 @@
-﻿Structure Bericht
-    Dim afzender As String
-    Dim datum As DateTime
-    Dim bericht As String
+﻿Class Bericht
+    Public afzender As String
+    Public datum As DateTime
+    Public bericht As String
 
     Public Overrides Function ToString() As String
         Return "Afzender: " & afzender & vbCrLf & "Bericht: " & bericht & vbCrLf & "Datum + tijd: " & datum
     End Function
-End Structure
+End Class
 
 Module Module1
 
     Sub Main()
 
-        Dim bericht1, bericht2 As Bericht
+        Dim bericht1, bericht2 As New Bericht
         Dim geboortedatum As New DateTime
 
         geboortedatum = Date.Parse("4 december")
