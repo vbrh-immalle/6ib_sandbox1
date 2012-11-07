f3287fbf3fc68378e89a7436864a264e08e985b9
Dinges <vincent.voortmans@immalle.be>
2012-11-07 09:37:05 +0100

    Update structures vs classes
    Update

--- OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Module1.vb
+++ OefStructuresClassesToString/VoVincent/StructureVsClass/StructureVsClass/Module1.vb
@@ -2,6 +2,10 @@
     Dim afzender As String
     Dim bericht As String
 
+    Public Overrides Function ToString() As String
+
+        Return "Afzender: " & afzender & vbCrLf & "Bericht: " & bericht
+    End Function
     ' Override hier de ToString() method van object die afzender en bericht afprint
     ' b.v. 
     ' Afzender: Jos
@@ -13,6 +17,18 @@ Module Module1
     Sub Main()
         ' Maak hier 2 nieuwe berichten aan en druk ze af met Console.WriteLine (en de ToString)-method
         ' Denk eraan: structures zijn VALUE types!
+        Dim b1 As Bericht
+        b1.afzender = "barry"
+        b1.bericht = "saispas"
+        Console.WriteLine(b1.ToString())
+
+        Dim b2 As Bericht
+        b2.afzender = "Walter"
+        b2.bericht = "dinges"
+        Console.WriteLine(b2.ToString())
+
+
+
 
     End Sub
 
