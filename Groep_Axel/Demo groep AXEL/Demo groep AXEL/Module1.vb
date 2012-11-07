96b88ef3e73b9871bf64397e0d84c4254e3345de
Robie Konijn nv <robin.janssens@immalle.be>
2012-11-07 09:57:03 +0100

    BMI
    Laatste versie van BMI-dinges

--- Groep_Axel/Demo groep AXEL/Demo groep AXEL/Module1.vb
+++ Groep_Axel/Demo groep AXEL/Demo groep AXEL/Module1.vb
@@ -1,19 +1,40 @@
 ﻿Module Module1
 
-    Function BerekenBMI(ByVal lengte As Double, ByVal gewicht As Double) As Double
-        Return gewicht / lengte ^ 2 * 10000
-    End Function
+    Public Class BMICalculator
 
-    Function BerekenBMIAsStrings(ByVal lengte As String, ByVal gewicht As String) As String
-        Return BerekenBMI(Double.Parse(lengte), Double.Parse(gewicht)).ToString()
-    End Function
+        Private _lengte As Double
+        Private _gewicht As Double
+
+        Public Sub New(ByVal strLengte As String, ByVal strGewicht As String)
+            _lengte = Double.Parse(strLengte)
+            _gewicht = Double.Parse(strGewicht)
+        End Sub
+
+        Public Sub New(ByVal dblLengte As Double, ByVal dblGewicht As Double)
+            _lengte = dblLengte
+            _gewicht = dblGewicht
+        End Sub
+
+        ReadOnly Property BMI()
+            Get
+                Return BerekenBMI(_lengte, _gewicht).ToString()
+            End Get
+        End Property
+
+        Private Function BerekenBMI(ByVal lengte As Double, ByVal gewicht As Double) As Double
+            Return gewicht / lengte ^ 2
+        End Function
+
+    End Class
 
     Sub Main()
-        Console.WriteLine("Geef eerst lengte daarna gewicht in.")
-        Console.WriteLine("bmi persoon 1: " & BerekenBMIAsStrings(Console.ReadLine(), Console.ReadLine()))
-        Console.WriteLine("bmi persoon 2: " & BerekenBMIAsStrings(Console.ReadLine(), Console.ReadLine()))
+        Dim a As New BMICalculator(1.7, 80)
+        Dim b As New BMICalculator(1.8, 80)
 
-        Console.ReadLine()
+        Console.WriteLine(a.BMI)
+        Console.WriteLine(b.BMI)
     End Sub
 
 End Module
+
+
