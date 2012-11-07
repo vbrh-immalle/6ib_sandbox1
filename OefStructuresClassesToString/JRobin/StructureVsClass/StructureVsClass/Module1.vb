052e03a321786c575679a58616c080dfeef4f8f2
immalle <hans.vanbroeckhoven@immalle.be>
2012-11-07 09:27:17 +0100

    Opgave Structure Vs Class
    Elke leerling krijgt identiek hetzelfde project dat moet aangevuld
    worden.

---
+++ OefStructuresClassesToString/JRobin/StructureVsClass/StructureVsClass/Module1.vb
@@ -0,0 +1,19 @@
+﻿Structure Bericht
+    Dim afzender As String
+    Dim bericht As String
+
+    ' Override hier de ToString() method van object die afzender en bericht afprint
+    ' b.v. 
+    ' Afzender: Jos
+    ' Bericht: Waar ben je?
+End Structure
+
+Module Module1
+
+    Sub Main()
+        ' Maak hier 2 nieuwe berichten aan en druk ze af met Console.WriteLine (en de ToString)-method
+        ' Denk eraan: structures zijn VALUE types!
+
+    End Sub
+
+End Module
