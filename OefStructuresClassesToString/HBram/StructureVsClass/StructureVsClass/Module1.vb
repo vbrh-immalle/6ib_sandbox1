0654cea972511c38109bb4b70e9c01fdf451e16b
Bram <bram.hanssens@immalle.be>
2012-11-07 09:39:15 +0100

    Oplossing

--- OefStructuresClassesToString/HBram/StructureVsClass/StructureVsClass/Module1.vb
+++ OefStructuresClassesToString/HBram/StructureVsClass/StructureVsClass/Module1.vb
@@ -2,15 +2,29 @@
     Dim afzender As String
     Dim bericht As String
 
+    Public Overrides Function ToString() As String
+        Dim rtrnwrd As String
+
+        rtrnwrd = "afzender: " & afzender & vbCrLf & "bericht: " & bericht & vbCrLf
+
+        Return rtrnwrd
+    End Function
+
     ' Override hier de ToString() method van object die afzender en bericht afprint
-    ' b.v. 
-    ' Afzender: Jos
-    ' Bericht: Waar ben je?
 End Structure
 
 Module Module1
 
     Sub Main()
+        Dim b1, b2 As Bericht
+
+        b1.afzender = "Jan"
+        b2.bericht = "Heey!"
+        b2.afzender = "Joseph"
+        b1.bericht = "Hallo!"
+
+        Console.WriteLine(b1)
+        Console.WriteLine(b2)
         ' Maak hier 2 nieuwe berichten aan en druk ze af met Console.WriteLine (en de ToString)-method
         ' Denk eraan: structures zijn VALUE types!
 
