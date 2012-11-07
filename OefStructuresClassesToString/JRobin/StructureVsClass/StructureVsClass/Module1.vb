fca4ca44d5279bc9dc8d7fe812e4a0f2cfca0bdd
Robie Konijn nv. <robin.janssens@immalle.be>
2012-11-07 09:36:56 +0100

    Verplaatsing project
    In de foute map, stond in map van een ander project

--- OefStructuresClassesToString/JRobin/StructureVsClass/StructureVsClass/Module1.vb
+++ OefStructuresClassesToString/JRobin/StructureVsClass/StructureVsClass/Module1.vb
@@ -2,6 +2,9 @@
     Dim afzender As String
     Dim bericht As String
 
+    Public Overrides Function ToString() As String
+        Return "Afzender:"
+    End Function
     ' Override hier de ToString() method van object die afzender en bericht afprint
     ' b.v. 
     ' Afzender: Jos
@@ -13,7 +16,9 @@ Module Module1
     Sub Main()
         ' Maak hier 2 nieuwe berichten aan en druk ze af met Console.WriteLine (en de ToString)-method
         ' Denk eraan: structures zijn VALUE types!
-
+        Dim woord As String
+        woord = "Hallo"
+        Console.WriteLine(woord.ToString())
     End Sub
 
 End Module
