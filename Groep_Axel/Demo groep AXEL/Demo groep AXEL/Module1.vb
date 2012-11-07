78033fbae005b3d4c6ee873dada4e24490ba2c1f
Bram <bram.hanssens@immalle.be>
2012-11-07 09:20:32 +0100

    BMI beter

--- Groep_Axel/Demo groep AXEL/Demo groep AXEL/Module1.vb
+++ Groep_Axel/Demo groep AXEL/Demo groep AXEL/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
 
     Function BerekenBMI(ByVal lengte As Double, ByVal gewicht As Double) As Double
-        Return gewicht / lengte ^ 2
+        Return gewicht / lengte ^ 2 * 10000
     End Function
 
     Function BerekenBMIAsStrings(ByVal lengte As String, ByVal gewicht As String) As String
